91c3200ad7b1b4722c7c62ae4d0224a91f935f2b
Tanvir Ahmed Raihan <raihan.tanvir961@gmail.com>
2024-02-18 13:40:12 +0600

    Commit For half work

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './Body.module.css';
+
+
+const Body = () => {
+  return (
+    <div className={classes.mainBody}>
+            Body
+    </div>
+  );
+};
+
+export default Body;
